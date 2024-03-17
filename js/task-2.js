6249b1dfd7580df66ade209cd9de6cfcc5d4cdb4
ziablyck <monechca@gmail.com>
2024-03-17 14:07:48 +0200

    add hw6

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class Storage {
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem)
+    }
+
+    
+    removeItem(itemToRemove) {  
+        this.#items.splice(this.#items.indexOf(itemToRemove), 1)
+    }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
